a020e97c609bdb3dfb59f7498e45b86214c2fd51
Chris Lamb <lamb.chrisr@gmail.com>
2014-10-08 21:24:17 -0400

    Login a user when testing controllers.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe WelcomeController do
 
   describe "GET 'home'" do
-    before { get 'home' }
+    let(:user) { User.create!(email: 'bender@gmail.com', password: 'testtest') }
+    before do
+      sign_in(user)
+      get 'home'
+    end
 
     it "returns http success" do
       expect(response).to be_success
